70ef888e87f59400d981cbbc4ff9a486bdc4bf29
LAKSHYA <llakshya63@gmail.com>
2023-03-28 14:18:25 +0530

    new project is added

--- components/ProjectsLink.js
+++ components/ProjectsLink.js
@@ -21,6 +21,17 @@ const ProjectsLink = () => {
                 <div className='font-medium text-[18px] ml-[15px] md:text-[20px] '>:</div>
                 <div className='font-medium text-[#ff4D41] hover:font-bold text-[18px] md:text-[20px]  underline'><Link target={"_blank"} href={"https://inotebook-five.vercel.app"}>Visit iNoteBook</Link></div>
             </div>
+            <div className='grid grid-cols-3 md:w-[70%] m-auto gap-4 py-[7px]'>
+                <div className='  '>
+                    <div className='font-bold text-[18px] md:text-[20px]'>YMusic</div>
+                    <div className='text-[14px]'>A project, in which we can save youtube videos and can be seen later according to category.
+                        We can make bucket for example education video, entertainment video etc.
+                        History will be saved and it can be deleted if we want.
+                        We can change the category of video and we can edit the details of video as well, dynamically.</div>
+                </div>
+                <div className='font-medium text-[18px] ml-[15px] md:text-[20px] '>:</div>
+                <div className='font-medium text-[#ff4D41] hover:font-bold text-[18px] md:text-[20px]  underline'><Link target={"_blank"} href={"https://ymusic.vercel.app/"}>Visit YMusic</Link></div>
+            </div>
         </div>
     )
 }
